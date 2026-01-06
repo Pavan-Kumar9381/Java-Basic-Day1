7e2eb73582128589f4691c206d9493f1e4e4717d
Pavan-Kumar9381 <ramgiripavankumar@gmail.com>
2026-01-06 11:14:05 +0530

    Variables

---
+++ VariablesDemo.java
@@ -0,0 +1,15 @@
+public class Variables{
+    public static void main(String[] args) {
+        int age = 25;
+        double salary = 50000.50;
+        char grade = 'A';
+        String name = "John";
+        boolean isEmployed = true;
+
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("Salary: " + salary);
+        System.out.println("Grade: " + grade);
+        System.out.println("Employed: " + isEmployed);
+    }
+}
